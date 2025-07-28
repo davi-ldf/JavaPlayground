1d54535c197276c8dd3876bf5048b9e2630010f7
Davi Lisboa <davifranca135@gmail.com>
2025-07-28 20:43:29 -0300

    Create While.java

---
+++ src/While.java
@@ -0,0 +1,16 @@
+public class While {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while (counter <= 5) {
+            System.out.println(counter);
+            counter++;
+        }
+
+        do {
+            System.out.println(counter);
+            counter++;
+        } while (counter < 10);
+
+    }
+}
